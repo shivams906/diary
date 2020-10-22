936717a1fc87cdd94d4acb30c658588987b04b70
Shivam Sharma <shivams906@gmail.com>
2020-10-22 18:31:24 +0530

    changed hardcoded urls to use url names

--- diary/tests/test_views.py
+++ diary/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import resolve
+from django.urls import resolve, reverse
 from django.contrib.auth import get_user_model
 from diary.views import home, add
 from diary.forms import EntryForm
@@ -10,24 +10,24 @@ User = get_user_model()
 
 class HomePageTest(TestCase):
     def test_diary_url_resolves_to_correct_view_function(self):
-        found = resolve("/diary/")
+        found = resolve(reverse("diary:home"))
         self.assertEqual(found.func, home)
 
     def test_unauthenticated_users_are_shown_about_template(self):
-        response = self.client.get("/diary/")
+        response = self.client.get(reverse("diary:home"))
         self.assertTemplateUsed(response, "diary/about.html")
 
     def test_authenticated_users_are_shown_home_template(self):
         user = User.objects.create_user(username="user", password="password123")
         self.client.force_login(user)
-        response = self.client.get("/diary/")
+        response = self.client.get(reverse("diary:home"))
         self.assertTemplateUsed(response, "diary/home.html")
 
     def test_home_view_shows_diary(self):
         user = User.objects.create_user(username="user", password="password123")
         self.client.force_login(user)
-        self.client.post("/diary/add/", {"text": 10 * "text"})
-        response = self.client.get("/diary/")
+        self.client.post(reverse("diary:add"), {"text": 10 * "text"})
+        response = self.client.get(reverse("diary:home"))
         data = response.content.decode("utf-8")
         self.assertIn(10 * "text", data)
 
@@ -39,7 +39,7 @@ class HomePageTest(TestCase):
 
         self.client.force_login(user1)
 
-        response = self.client.get("/diary/")
+        response = self.client.get(reverse("diary:home"))
         entries = response.context["entries"]
         self.assertIn(entry1, entries)
         self.assertNotIn(entry2, entries)
@@ -52,40 +52,40 @@ class AddPageTest(TestCase):
 
     def test_add_url_resolves_to_correct_view_function(self):
         self.client.force_login(self.user)
-        found = resolve("/diary/add/")
+        found = resolve(reverse("diary:add"))
         self.assertEqual(found.func, add)
 
     def test_add_view_uses_correct_template(self):
         self.client.force_login(self.user)
-        response = self.client.get("/diary/add/")
+        response = self.client.get(reverse("diary:add"))
         self.assertTemplateUsed(response, "diary/add.html")
 
     def test_redirects_to_login_page_for_unauthenticated_users(self):
-        response = self.client.get("/diary/add/")
+        response = self.client.get(reverse("diary:add"))
         self.assertRedirects(response, "/accounts/login/?next=/diary/add/")
 
     def test_add_view_uses_correct_form(self):
         self.client.force_login(self.user)
-        response = self.client.get("/diary/add/")
+        response = self.client.get(reverse("diary:add"))
         self.assertIsInstance(response.context["form"], EntryForm)
 
     def test_POST_creates_an_entry(self):
         self.client.force_login(self.user)
-        self.client.post("/diary/add/", {"text": 10 * "text"})
+        self.client.post(reverse("diary:add"), {"text": 10 * "text"})
         self.assertEqual(Entry.objects.count(), 1)
 
     def test_POST_does_not_create_blank_diary(self):
         self.client.force_login(self.user)
-        self.client.post("/diary/add/", {"text": ""})
+        self.client.post(reverse("diary:add"), {"text": ""})
         self.assertEqual(Entry.objects.count(), 0)
 
     def test_valid_POST_redirects_to_home_page(self):
         self.client.force_login(self.user)
-        response = self.client.post("/diary/add/", {"text": 10 * "text"})
-        self.assertRedirects(response, "/diary/")
+        response = self.client.post(reverse("diary:add"), {"text": 10 * "text"})
+        self.assertRedirects(response, reverse("diary:home"))
 
     def test_current_user_is_saved_as_owner(self):
         self.client.force_login(self.user)
-        self.client.post("/diary/add/", {"text": 10 * "text"})
+        self.client.post(reverse("diary:add"), {"text": 10 * "text"})
         entry = Entry.objects.first()
         self.assertEqual(entry.owner, self.user)
